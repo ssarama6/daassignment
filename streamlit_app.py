204f1c2bf036429e1d611b706e4db4029801e555
ssarama6 <ssarama@charlotte.edu>
2025-03-30 20:02:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ st.bar_chart(df, x="Category", y="Sales")
 st.dataframe(df.groupby("Category").sum())
 # Using as_index=False here preserves the Category as a column.  If we exclude that, Category would become the datafram index and we would need to use x=None to tell bar_chart to use the index
 st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
+category_selected = st.selectbox("Select a Category", df['Category'].unique())
 
 # Aggregating by time
 # Here we ensure Order_Date is in datetime format, then set is as an index to our dataframe
@@ -35,9 +36,6 @@ st.write("### (2) add a multi-select for Sub_Category *in the selected Category 
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
-
-#1
-category_selected = st.selectbox("Select a Category", df['Category'].unique())
 
 #2
 filtered_df = df[df['Category'] == category_selected]
